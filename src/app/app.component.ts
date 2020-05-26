78dc24e317d01ae36c5a6178ab7f045bdb9de5e6
UI-Develop <bhagyashrideshpande09@gmail.com>
2020-05-26 18:35:17 +0530

    Pwa demo repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,3 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'PWA-poc';
-// }
 import { Component, OnInit, HostListener } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
 @Component({
